4a7e3d010bc1ea57901dca708317597e2a8abf1c
Tom Kirby <rtkirby@mac.com>
2016-01-15 15:27:35 -0700

    basic addition of code from AJAX
    /jquery.getjson/ code from web page to get a starting point

--- js/script.js
+++ js/script.js
@@ -22,6 +22,19 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     // YOUR CODE GOES HERE!
+    // NYTimes AJAX request
+    
+    $.getJSON( "ajax/test.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
 
     return false;
 };
